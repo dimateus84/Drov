55af464cc3b06356b2b80077265a14d9d6d72975
dimateus84 <stoppasivcom@yandex.ua>
2016-02-09 14:51:40 +0300

    edit b-info img

--- index.php
+++ index.php
@@ -2,141 +2,142 @@
 <?php include 'tpl/includes/head.inc'; ?>
 <body class="front page page-home">
 <div class="outer-wrapper">
-  <?php include 'tpl/layout/header.inc'; ?>
+<?php include 'tpl/layout/header.inc'; ?>
 
-  <div class="section section-top">
+<div class="section section-top">
 
-    <div class="img">
-      <img src="theme/images/bg/header-bg.jpg" alt="header"/>
+  <div class="img">
+    <img src="theme/images/bg/header-bg.jpg" alt="header"/>
+  </div>
+
+  <div class="site-container">
+
+    <div class="logo">
+      <a href="#"><img src="theme/images/logo.png" alt="logo"/>
+      </a>
     </div>
 
-    <div class="site-container">
+    <h1>Total Wheel Management System </h1>
 
-      <div class="logo">
-        <a href="#"><img src="theme/images/logo.png" alt="logo"/>
-        </a>
-      </div>
-
-      <h1>Total Wheel Management System </h1>
-
-      <div class="text">
-        <p>Ensure that optimal PSI is maintained at all times, dynamically,
-          with the Drov Total Wheel Management System.</p>
-      </div>
-
+    <div class="text">
+      <p>Ensure that optimal PSI is maintained at all times, dynamically,
+        with the Drov Total Wheel Management System.</p>
     </div>
 
   </div>
 
-  <div class="inner-wrapper">
-    <div class="content-wrapper site-container">
+</div>
 
-      <div class="b-links">
-        <div class="item-wrapper">
-          <div class="item">
-            <div class="img">
-              <img src="theme/images/tmp/header-col-1.png" alt=""/>
-            </div>
-            <h2>DR<span class="o">O</span>V TIRE
-              INFLATION</h2>
+<div class="inner-wrapper">
+  <div class="content-wrapper site-container">
+
+    <div class="b-links">
+      <div class="item-wrapper">
+        <div class="item">
+          <div class="img">
+            <img src="theme/images/tmp/header-col-1.png" alt=""/>
           </div>
-          <div class="item">
-            <img src="theme/images/tmp/header-col-2.png" alt=""/>
+          <h2>DR<span class="o">O</span>V TIRE
+            INFLATION</h2>
+        </div>
+        <div class="item">
+          <img src="theme/images/tmp/header-col-2.png" alt=""/>
 
-            <h2>
-              DR<span class="o">O</span>V
-              INTELLIGENT
-              SYSTEM</h2>
-          </div>
-          <div class="item">
-            <img src="theme/images/tmp/header-col-3.png" alt=""/>
+          <h2>
+            DR<span class="o">O</span>V
+            INTELLIGENT
+            SYSTEM</h2>
+        </div>
+        <div class="item">
+          <img src="theme/images/tmp/header-col-3.png" alt=""/>
 
-            <h3><span>
+          <h3><span>
             DR<span class="o">O</span>V
             ANALYTICS </span>
-            </h3>
+          </h3>
 
-            <div class="text">
-              Logistics Productivity & Analysis
-            </div>
-
-            <ul>
-              <li>Tire RIO</li>
-              <li>Compliance</li>
-              <li>Fuel Effeciency</li>
-              <li>Fleet Utilization & KPI’s</li>
-              <li>Vendor Comparative Analysis</li>
-              <li>Preventive Maintenance Effectiveness</li>
-            </ul>
-
+          <div class="text">
+            Logistics Productivity & Analysis
           </div>
-        </div>
 
+          <ul>
+            <li>Tire RIO</li>
+            <li>Compliance</li>
+            <li>Fuel Effeciency</li>
+            <li>Fleet Utilization & KPI’s</li>
+            <li>Vendor Comparative Analysis</li>
+            <li>Preventive Maintenance Effectiveness</li>
+          </ul>
+
+        </div>
       </div>
 
+    </div>
 
-      <div class="b-slider ">
-        <h4>Dr<span class="o">o</span>v Intelligent System</h4>
 
-        <div class="slider-d">
-          <div class="flexslider">
-            <ul class="slides">
-              <li>
-                <img src="theme/images/tmp/slider-img-1.jpg" alt=""/>
+    <div class="b-slider ">
+      <h4>Dr<span class="o">o</span>v Intelligent System</h4>
 
-                <div class="desc">All wheels are
-                  individually scanned
-                  for real-time operation.
-                </div>
-              </li>
-              <li>
+      <div class="slider-d">
+        <div class="flexslider">
+          <ul class="slides">
+            <li>
+              <img src="theme/images/tmp/slider-img-1.jpg" alt=""/>
 
-                <img src="theme/images/tmp/slider-img-2.jpg" alt=""/>
+              <div class="desc">All wheels are
+                individually scanned
+                for real-time operation.
+              </div>
+            </li>
+            <li>
 
-                <div class="desc">When faced with
-                  an issue...
-                </div>
-              </li>
-              <li>
+              <img src="theme/images/tmp/slider-img-2.jpg" alt=""/>
 
-                <img src="theme/images/tmp/slider-img-3.jpg" alt=""/>
+              <div class="desc">When faced with
+                an issue...
+              </div>
+            </li>
+            <li>
 
-                <div class="desc">Onboard Wheel Logic
-                  (OWL) computer determines the situation
-                </div>
-              </li>
-              <li>
+              <img src="theme/images/tmp/slider-img-3.jpg" alt=""/>
 
-                <img src="theme/images/tmp/slider-img-4.jpg" alt=""/>
+              <div class="desc">Onboard Wheel Logic
+                (OWL) computer determines the situation
+              </div>
+            </li>
+            <li>
 
-                <div class="desc">and can inflate or deflate tires.</div>
-              </li>
-              <li>
+              <img src="theme/images/tmp/slider-img-4.jpg" alt=""/>
 
-                <img src="theme/images/tmp/slider-img-5.jpg" alt=""/>
+              <div class="desc">and can inflate or deflate tires.</div>
+            </li>
+            <li>
 
-                <div class="desc">Appropriate information is sent to logistics center</div>
-              </li>
-              <li>
+              <img src="theme/images/tmp/slider-img-5.jpg" alt=""/>
 
-                <img src="theme/images/tmp/slider-img-6.jpg" alt=""/>
+              <div class="desc">Appropriate information is sent to logistics center</div>
+            </li>
+            <li>
 
-                <div class="desc">OWL determines if driver action is necessary</div>
-              </li>
-              <li>
+              <img src="theme/images/tmp/slider-img-6.jpg" alt=""/>
 
-                <img src="theme/images/tmp/slider-img-7.jpg" alt=""/>
+              <div class="desc">OWL determines if driver action is necessary</div>
+            </li>
+            <li>
 
-                <div class="desc">Drov Analytics provides wheel history and other maintenance data</div>
-              </li>
-            </ul>
-          </div>
+              <img src="theme/images/tmp/slider-img-7.jpg" alt=""/>
+
+              <div class="desc">Drov Analytics provides wheel history and other maintenance data</div>
+            </li>
+          </ul>
         </div>
-
       </div>
 
-      <div class="b-desc">
+    </div>
 
+    <div class="b-desc">
+
+      <div class="site-container">
         <h5>WE PUT OUR BEST WHEEL FORWARD.</h5>
 
         <div class="text">Lorem ipsum dolor sit amet,
@@ -148,61 +149,62 @@
           eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
           non proident,
         </div>
-
       </div>
-
-      <div class="b-info">
-        <div class="content-item">
-          <div class="desc">
-            <h5>MAINTAINING
-              IDEAL PSI</h5>
-
-            <div class="text">
-              <p>Sensors capture key information and automatically adjust the tire pressure to match the ideal PSI
-                recommended by the tire manufacturer... in real time.
-              </p>
-
-              <ul>
-                <li>Load</li>
-                <li>Temperature</li>
-                <li>Environment</li>
-              </ul>
-              <div class="img">
-                <img src="theme/images/tmp/ideal-psi.jpg" alt="ideal"/>
-              </div>
-            </div>
-          </div>
-          <div class="img">
-            <img src="theme/images/tmp/ideal-psi-graffik.jpg" alt="graffik"/>
-          </div>
-        </div>
-        <div class="content-item">
-          <div class="desc">
-            <h5>VALUE PROPOSITION</h5>
-
-            <div class="text">
-              <p>By maintaining proper PSI at all times, vehicle owners will realize the Value represented by the Tire
-                OEM:
-              </p>
-
-              <ul>
-                <li>Fuel Efficiency Gains / Compliance</li>
-                <li>Tire Life / ROI</li>
-                <li>Safety</li>
-              </ul>
-
-            </div>
-          </div>
-          <div class="img">
-            <img src="theme/images/tmp/value-proposition.jpg" alt="value"/>
-          </div>
-        </div>
-      </div>
-
 
     </div>
+
+    <div class="b-info">
+      <div class="content-item">
+        <div class="desc">
+          <h5>MAINTAINING
+            IDEAL PSI</h5>
+
+          <div class="text">
+            <p>Sensors capture key information and automatically adjust the tire pressure to match the ideal PSI
+              recommended by the tire manufacturer... in real time.
+            </p>
+
+            <ul>
+              <li>Load</li>
+              <li>Temperature</li>
+              <li>Environment</li>
+            </ul>
+            <div class="img">
+              <img src="theme/images/tmp/ideal-psi.jpg" alt="ideal"/>
+            </div>
+          </div>
+        </div>
+        <div class="img">
+          <img src="theme/images/tmp/ideal-psi-graffik.jpg" alt="graffik"/>
+        </div>
+      </div>
+      <div class="content-item">
+        <div class="desc">
+          <h5>VALUE PROPOSITION</h5>
+
+          <div class="text">
+            <p>By maintaining proper PSI at all times, vehicle owners will realize the Value represented by the Tire
+              OEM:
+            </p>
+
+            <ul>
+              <li>Fuel Efficiency Gains / Compliance</li>
+              <li>Tire Life / ROI</li>
+              <li>Safety</li>
+            </ul>
+
+          </div>
+        </div>
+        <div class="img">
+          <img src="theme/images/tmp/value-proposition.jpg" alt="value"/>
+        </div>
+      </div>
+    </div>
+
+
   </div>
-  <?php include 'tpl/layout/footer.inc'; ?>
+</div>
+<?php include 'tpl/layout/footer.inc'; ?>
 </div>
 </body>
 </html>
